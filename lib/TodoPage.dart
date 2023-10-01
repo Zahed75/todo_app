5838ff8d68c15c8a818d018bd8ef51c9148d6dac
“Zahed75” <zahedhasan.cs@gmail.com>
2023-10-01 14:53:58 +0600

    Todo Page done and Main Page Done

--- lib/TodoPage.dart
+++ lib/TodoPage.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-class TodoPage extends StatefulWidget{
+class TodoPage extends StatefulWidget {
   @override
-  State<StatefulWidget>createState(){
-    
+  State<StatefulWidget> createState() {
     return TodoPageView();
   }
 }
 
-
-class TodoPageView extends State<TodoPage>{
+class TodoPageView extends State<TodoPage> {
+  List TodoList=[];
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title:Text("Todo")
-      ),
-    
-    );
+        appBar: AppBar(title: Text("Todo")),
+        body: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Expanded(
+                  flex:10,
+                  child: Row(
+                    children: [
+                      Expanded(child:TextFormField()),
+                      Expanded(child:ElevatedButton(onPressed: (){},child:Text("Add"))),
+                    ],
+                  )
+                  
+                  ),
+                Expanded(
+                  flex:90,
+                  child:ListView.builder(
+                    itemCount: TodoList.length,
+                    itemBuilder: (context,index){
+                      return Card(
+                        
+                      )
+                    }
+                  )
+                  
+                  ),
+              ],
+            )
+            )
+            );
   }
 }
